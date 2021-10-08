87806eadd31bcc00dde0395c39fe1e1696590bd6
augustine jimmy <augustinej1436@gmail.com>
2021-10-08 11:22:15 +0530

    SBA1

---
+++ training/SBA/SBA1/SBA2inputconsole.java
@@ -0,0 +1,18 @@
+package com.ust.test;
+import java.util.Scanner;
+
+
+public class SBA2inputconsole{
+    public static void main(String args[]) {
+        int number,sum=0;
+        Scanner input=new Scanner(System.in);
+
+        System.out.println("Enter your limit");
+         number=input.nextInt();
+         for(int i=number;i>=0;i--) {
+        		  sum=sum+i; 
+        	  }
+                
+           System.out.println("Sum of positive numbers"+sum);
+}
+}
